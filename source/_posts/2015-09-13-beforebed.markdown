38eb2ebd3537a4930008f79bcef269926a6db977
jjm61 <jjm61@cam.ac.uk>
2015-09-14 00:09:08 +0100

    Added stretching routine

---
+++ source/_posts/2015-09-13-beforebed.markdown
@@ -0,0 +1,58 @@
+---
+layout: workoutApp
+title:  "Before Bed Stretch"
+date:   2015-09-13 18:43:00
+categories: stretching js
+description: All round 15 minute stretching routine to relax you into sleeping.
+
+---
+
+<script type="text/javascript">
+    function get_exercises(){
+        var elibrary = exerciseLibrary();
+        var slibrary = stretchLibrary();
+        var exercises = [];
+
+        for(var i=0;i<2;i++){
+            exercises.push({exercise: elibrary.trunkRotations, time: 20, reps: 0});
+            exercises.push({exercise: slibrary.headCircles, time: 20, reps: 0});
+            exercises.push({exercise: elibrary.armCircles, time: 20, reps: 0});
+        }
+        exercises.push({exercise: slibrary.leftandRightHeadStretch, time: 20, reps: 0});
+        exercises.push({exercise: slibrary.leftandRightHeadStretch, time: 20, reps: 0});
+                         
+        //Standing legs
+        exercises.push({exercise: slibrary.leftShortCalfStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightShortCalfStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.leftLongCalfStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightLongCalfStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.standingToeTouches,  time: 30, reps: 0});
+        exercises.push({exercise: slibrary.boxSplits, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.leftHipFlexors, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightHipFlexors, time: 30, reps: 0});
+
+        //Standing arms
+        exercises.push({exercise: slibrary.leftShoulderStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightShoulderStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.leftTricepStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightTricepStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.upwardStretch, time: 30, reps: 0});
+
+        //Sitting legs and back
+        exercises.push({exercise: slibrary.catStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.leftSittingTwist, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightSittingTwist, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.butterflyStretch, time: 30, reps: 0});
+
+        //Laying legs and back
+        exercises.push({exercise: slibrary.leftLayingQuadStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightLayingQuadStretch, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.leftLayingKneeToChest, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightLayingKneeToChest, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.leftLayingTwist, time: 30, reps: 0});
+        exercises.push({exercise: slibrary.rightLayingTwist, time: 30, reps: 0});
+
+
+        return exercises;
+    }
+</script>
---
+++ source/js/stretches.js
@@ -0,0 +1,122 @@
+---
+---
+/*Exercises included are
+ -    boxsplits:
+ */
+
+function stretchLibrary(){
+    
+    return {
+        headCircles:
+            {name: "Head Circles",
+            image: "",
+            description: ""},
+        fourCounersHeadStretch:
+            {name: "Four Corner Head Stretch",
+            image: "",
+            description: ""},
+        forwardAndBackHeadStretch:
+            {name: "Forward and Back Head Stretch",
+            image: "",
+            description: ""},
+        leftAndRightHeadStretch:
+            {name: "Left and Right Head Stretch",
+            image: "",
+            description: ""},
+        leftShoulderStretch:
+            {name: "Shoulder Stretch (Left)",
+            image: "",
+            description: ""},
+        rightShoulderStretch:
+            {name: "Shoulder Stretch (Right)",
+            image: "",
+            description: ""},
+        leftTricepStretch:
+            {name: "Tricep Stretch (Right)",
+            image: "",
+            description: ""},
+        rightTricepStretch:
+            {name: "Tricep Stretch (Right)",
+            image: "",
+            description: ""},
+        upwardStretch:
+            {name: "Upward Stretch",
+            image: "",
+            description: "Interlock fingres, palms away, and raise arms above head."},
+        boxSplits:
+            {name: "Box Splits",
+            image: "",
+            description: "Legs out sideways."},
+        catstretch:
+            {name: "Cat Stretch",
+            image: "",
+            description: "On your hands and knees, flex and contract your back."},
+        leftSittingTwist:
+            {name: "Sitting Twist (Left)",
+            image: "",
+            description: ""},
+        rightSittingTwist:
+            {name: "Sitting Twist (Right)",
+            image: "",
+            description: ""},
+        leftLayingTwist:
+            {name: "Laying Twist (Left)",
+            image: "",
+            description: ""},
+        rightLayingTwist:
+            {name: "Laying Twist (Right)",
+            image: "",
+            description: ""},
+        leftLayingQuad:
+            {name: "Laying Quad Stretch (Left)",
+            image: "",
+            description: ""},
+        rightLayingQuad:
+            {name: "Laying Quad Stretch (Right)",
+            image: "",
+            description: ""},
+        standingToeTouchesStretch:
+            {name: "Standing Toe Touches",
+            image: "",
+            description: ""},
+        seatingToeTouchesStretch:
+            {name: "Seating Toe Touches",
+            image: "",
+            description: ""},
+        butterflyStretch:
+            {name: "Butterfly Stretch",
+            image: "",
+            description: ""},
+        seatingStraddle:
+            {name: "Seating Straddle",
+            image: "",
+            description: ""},
+        leftShortCalfStretch:
+            {name: "Short Calf Stretch (Left)",
+            image: "",
+            description: ""},
+        rightShortCalfStretch:
+            {name: "Short Calf Stretch (Right)",
+            image: "",
+            description: ""},
+        leftStandingHipFlexors:
+            {name: "Standing Hip Flexors (Left)",
+            image: "",
+            description: ""},
+        rightStandingHipFlexors:
+            {name: "Standing Hip Flexors (Right)",
+            image: "",
+            description: ""},
+        leftLayingKneeToChest:
+            {name: "Laying Knee to Chest (Left)",
+            image: "",
+            description: ""},
+        rightLayingKneeToChest:
+            {name: "Laying Knee to Chest (Right)",
+            image: "",
+            description: ""},
+
+    }
+
+
+}
